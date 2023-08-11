769522778770e3826b032940a1de14c13bbb2063
SculptorLux <alexanisin@gmail.com>
2023-08-11 18:21:19 +0300

    3.8 Наследование шаблонов Django. Теги block и extends Все задания блока 3.8 Проект «Мой блог»
    Как  правило все статьи сразу не выводят на главной странице, а только некоторые из них. Например, самые популярные или самые последние. Мы же будем выводить три рандомные статьи. Для этого измените ваш код так, чтобы при каждом обращении к главной странице отображались 3 случайных элемента из списка posts(он был создан на предыдущем уроке)

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.posts_info),
-    path('keanu/', views.keanu),
+    path('keanu', views.keanu),
     path('guinness_world_records', views.get_guinness_world_records),
     path('<int:number_post>', views.personal_post_number),
     path('<str:name_post>', views.personal_post),
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,52 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-from django.template.loader import render_to_string
+from django.shortcuts import render
+from random import sample
+
+posts = [
+    {
+        'title': 'Рыбалка',
+        'description': 'Хорошо посидели',
+        'date': '21 авг 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'
+    },
+    {
+        'title': 'Париж',
+        'description': 'Незабываемое путешествие',
+        'date': '5 сент 2020',
+        'content': '''Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'''
+    },
+    {
+        'title': 'Финал лиги чемпионов',
+        'description': 'Реал опять выиграл ЛЧ',
+        'date': '28 мая 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, rem.'
+    },
+    {
+        'title': 'Охота на уток',
+        'description': 'Ни одна утка не пострадала',
+        'date': '7 дек 2019',
+        'content': 'Lorem ipsum dolor sit amet.'
+    },
+    {
+        'title': 'Фестиваль огурца',
+        'description': 'Суздаль ждет тебя',
+        'date': '12 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consectetur id inventore odit recusandae!'
+    },
+    {
+        'title': 'Нашествие',
+        'description': 'Даешь рок, но в следующем году',
+        'date': '29 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia recusandae rem?'
+    },
+    {
+        'title': 'Новый год',
+        'description': 'Эх, еще один год пролетел',
+        'date': '31 дек 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto corporis fuga ipsam laboriosam, nesciunt non quae qui ut veniam.'
+    },
+]
 
 
 def posts_info(request):
@@ -10,8 +54,11 @@ def posts_info(request):
 
 
 def main(request):
-    response = render_to_string('blog/index.html')
-    return HttpResponse(response)
+    postr_random = sample(posts, 3)
+    context = {
+        'posts': postr_random
+    }
+    return render(request, 'blog/index.html', context)
 
 
 def personal_post(request, name_post):
